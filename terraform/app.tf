14ba247932756fa931dbc3697a5bcead3a670abf
Roman Trufanov <roman.trufanov@waveaccess.global>
2023-10-07 13:53:50 +0400

    fix: remove mongo resource

--- terraform/app.tf
+++ terraform/app.tf
@@ -1,7 +1,7 @@
 locals {
   bot_env = join(" ", [
     "-e TelegramBotAccessToken=${var.telegram_bot_token}",
-    "-e ConnectionStrings__MongoDbConnectionString=\"mongodb://${var.mongo_user}:${var.mongo_password}@mongo:27017\""
+    "-e ConnectionStrings__MongoDbConnectionString=\"mongodb://${var.mongo_user}:${var.mongo_password}@localhost:27017\""
   ])
 }
 
@@ -9,9 +9,6 @@ resource "null_resource" "up_bot_container" {
   triggers = {
     build_number = timestamp()
   }
-  depends_on = [
-    null_resource.up_mongodb
-  ]
 
   connection {
     user        = "root"
@@ -23,7 +20,6 @@ resource "null_resource" "up_bot_container" {
   provisioner "remote-exec" {
     inline = [
       "docker container rm -f bot &> /dev/null",
-      "docker pull pingvin1308/cleannetcode.bot:${var.image_version}",
       "docker run -d ${local.bot_env} --name bot -v /bot_data/Data:/app/Data -v /bot_data/FileStorage:/app/FileStorage pingvin1308/cleannetcode.bot:${var.image_version}",
       "docker container ls"
     ]
