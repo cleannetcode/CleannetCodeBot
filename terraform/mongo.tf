202786d6d8078c9e2f56539245a805eb8c2dc4ec
Roman Trufanov <roman.trufanov@waveaccess.global>
2023-10-07 13:46:57 +0400

    fix: edit docker mongo run command

--- terraform/mongo.tf
+++ terraform/mongo.tf
@@ -18,7 +18,7 @@ resource "null_resource" "up_mongodb" {
   provisioner "remote-exec" {
     inline = [
       "docker pull mongo:6.0",
-      "docker run -d -e MONGO_INITDB_ROOT_USERNAME=${var.mongo_user} -e MONGO_INITDB_ROOT_PASSWORD=${var.mongo_password} -v /bot_data/mongo:/etc/mongo mongo:6.0",
+      "docker run --name mongo -d -e MONGO_INITDB_ROOT_USERNAME=${var.mongo_user} -e MONGO_INITDB_ROOT_PASSWORD=${var.mongo_password} -p 27017:27017 -v /bot_data/mongo:/etc/mongo mongo:6.0 --auth",
       "docker container ls"
     ]
   }
