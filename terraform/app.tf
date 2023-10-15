32ecf657f99d86ab83c3632f11e11fafbb91941f
Roman Trufanov <PingViN1308@gmail.com>
2023-10-15 16:40:33 +0400

    fix: Update app.tf

--- terraform/app.tf
+++ terraform/app.tf
@@ -22,7 +22,7 @@ resource "null_resource" "up_bot_container" {
     inline = [
       "docker container stop bot &> /dev/null",
       "docker container rm bot &> /dev/null",
-      "docker rmi $(docker images | grep 'cleannetcode.bot')",
+      "docker rmi $(docker images | grep 'cleannetcode.bot') &> /dev/null",
       "docker pull pingvin1308/cleannetcode.bot:${var.image_version}",
       "echo docker run -d -e TelegramBotAccessToken=${var.telegram_bot_token} -e ConnectionStrings__MongoDbConnectionString=${local.mongo_connection_string} --name bot -v /bot_data/Data:/app/Data -v /bot_data/FileStorage:/app/FileStorage pingvin1308/cleannetcode.bot:${var.image_version} >> test.md",
       "docker run -d -e TelegramBotAccessToken=${var.telegram_bot_token} --name bot -v /bot_data/Data:/app/Data -v /bot_data/FileStorage:/app/FileStorage pingvin1308/cleannetcode.bot:${var.image_version}",
