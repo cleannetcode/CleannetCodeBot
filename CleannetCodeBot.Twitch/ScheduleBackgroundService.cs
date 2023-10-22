fafe3f5fda0c7429a1310e3c71b50665344722c5
rpxgmptmcb <rpxgmptmcb@outlook.com>
2023-10-22 03:31:00 +0300

    refactor: change schedule service to periodic timer
    add logging to schedule service

--- CleannetCodeBot.Twitch/ScheduleBackgroundService.cs
+++ CleannetCodeBot.Twitch/ScheduleBackgroundService.cs
@@ -9,36 +9,54 @@ public class ScheduleBackgroundService : BackgroundService
     private readonly IUsersPollStartRegistry _usersPollStartRegistry;
     private readonly IPollsService _pollsService;
     private readonly IMemoryCache _memoryCache;
+    private readonly ILogger<ScheduleBackgroundService> _logger;
+    
+    private readonly TimeSpan _timerResolution = TimeSpan.FromMinutes(1); 
 
     public ScheduleBackgroundService(IPollsRepository pollsRepository, 
         IUsersPollStartRegistry usersPollStartRegistry,
         IPollsService pollsService, 
-        IMemoryCache memoryCache)
+        IMemoryCache memoryCache,
+        ILogger<ScheduleBackgroundService> logger)
     {
         _pollsRepository = pollsRepository;
         _usersPollStartRegistry = usersPollStartRegistry;
         _pollsService = pollsService;
         _memoryCache = memoryCache;
+        _logger = logger;
     }
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        while (!stoppingToken.IsCancellationRequested)
+        _logger.LogInformation("Timed Hosted Service running.");
+        
+        using PeriodicTimer timer = new(_timerResolution);
+
+        while (await timer.WaitForNextTickAsync(stoppingToken))
         {
-            await Task.Delay(1_000, stoppingToken);
-            var authToken = _memoryCache.Get<AuthToken>(AuthToken.Key);
-
-            if (authToken is not null)
-            {
-                var expiredPolls = _pollsRepository.GetExpiredPolls(DateTime.UtcNow);
-
-                async void CloseExpiredPolls(Poll x) =>
-                    await _pollsService.ClosePoll(x.RewardId, x.BroadcasterId, authToken.AccessToken);
-
-                expiredPolls.ForEach(CloseExpiredPolls);
-            }
-
-            _usersPollStartRegistry.ClearExpiredRecords(DateTime.UtcNow);
+            await DoWork();
         }
+
+        _logger.LogInformation("Timed Hosted Service is stopping.");
+    }
+
+    private async Task DoWork()
+    {
+        var authToken = _memoryCache.Get<AuthToken>(AuthToken.Key);
+
+        if (authToken is not null)
+        {
+            _logger.LogInformation("Searching for expired polls");
+            var expiredPolls = _pollsRepository.GetExpiredPolls(DateTime.UtcNow);
+            _logger.LogInformation($"Found {expiredPolls.Count} expired polls");
+
+            foreach (var expiredPoll in expiredPolls)
+            {
+                await _pollsService.ClosePoll(expiredPoll.RewardId, expiredPoll.BroadcasterId, authToken.AccessToken);
+            }
+        }
+
+        _logger.LogInformation($"Clear expired user records at {DateTime.UtcNow}");
+        _usersPollStartRegistry.ClearExpiredRecords(DateTime.UtcNow);
     }
 }
